a3ea5f0f017972431a88e7a7e7a458f2635f89e2
Aryainguz <aryaninguz369@gmail.com>
2023-11-13 23:08:30 +0530

    FIxed subtotal bug

--- src/Home.jsx
+++ src/Home.jsx
@@ -135,8 +135,8 @@ const [sum, setSum] = useState(0)
         {/* Sub total */}
         <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
           <div className="mb-2 flex justify-between">
+            <p className="text-gray-700">SubTotal</p>
             <p className="text-gray-700">{sum}</p>
-            <p className="text-gray-700">$129.99</p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-700">Shipping</p>
